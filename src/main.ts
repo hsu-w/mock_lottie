efc221b06083bed957278df1582830de78c4c7c8
許維哲 <hsu.w@nadia.bz>
2026-03-09 02:31:15 +0900

    [update] 初期設定

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import "./reset.css";
+import "./style.css";
+import lottie from "lottie-web";
+const menuBtn = document.getElementById("lottie-menu") as HTMLElement;
+const menuAnimation = lottie.loadAnimation({
+  container: menuBtn!, // the DOM element
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  path: "data.json", // the path to the animation json file
+});
+menuAnimation.setSpeed(2);
+
+menuBtn.addEventListener("click", () => {
+  if (menuBtn.dataset.state == "off") {
+    menuBtn.dataset.state = "on";
+    lottie.setDirection(1);
+    lottie.play();
+  } else {
+    menuBtn.dataset.state = "off";
+    lottie.setDirection(-1);
+    lottie.play();
+  }
+});
